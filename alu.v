15be79c6e58f3ae198a76c02f47c0d9ac9608f35
Patrick Colque <patrick.colque@utec.edu.pe>
2026-09-17 00:12:27 -0500

    Inputs y outputs de 5 bits

--- alu.v
+++ alu.v
@@ -1,32 +1,31 @@
 //32-bit ALU for ARM processor
-module alu(input  [31:0] a, b,
+module alu(input  [4:0] a, b, //Entradas de 5 bits
            input  [2:0]  ALUControl,
-           output reg [31:0] Result,
+           output reg [4:0] Result, //Salida de 5 bits
            output wire [3:0]  ALUFlags);
 
   wire neg, zero, carry, overflow;
-  wire [31:0] condinvb;
-  wire [32:0] sum;
+  wire [4:0] condinvb;
+  wire [5:0] sum;
 
   assign condinvb = ALUControl[0] ? ~b : b;
   assign sum = a + condinvb + ALUControl[0];
 
   always @(*)
     begin
-      case (ALUControl[2:0])
-        3'b000: Result = sum;
-        3'b001: Result = sum;
+      casex (ALUControl[2:0])
+        3'b00?: Result = sum;
         3'b010: Result = a & b;
         3'b011: Result = a | b;
-        3'b100: Result = a ^ b;
-        default: Result = 32'b0;
+        3'b110: Result = a ^ b; //Cambio en la forma de ver si es xor
+        default: Result = 5'b0;
       endcase
     end
 
-  assign neg = Result[31];
-  assign zero = (Result == 32'b0);
-  assign carry = (ALUControl[2:1] == 2'b00) & sum[32];
-  assign overflow = (ALUControl[2:1] == 2'b00) & ~(a[31] ^ b[31] ^ ALUControl[0]) & (a[31] ^ sum[31]);
+  assign neg = Result[4];
+  assign zero = (Result == 5'b0);
+  assign carry = (ALUControl[1] == 1'b0) & sum[5]; //ALUControl[1] = 0 => aritmetico
+  assign overflow = (ALUControl[1] == 1'b0) & ~(a[4] ^ b[4] ^ ALUControl[0]) & (a[4] ^ sum[4]);
   assign ALUFlags = {neg, zero, carry, overflow};
 
 endmodule
--- shift.v
+++ shift.v
@@ -1,7 +1,7 @@
 //32-bit left shift unit
-module shift(input  [31:0] a,
+module shift(input  [4:0] a,
              input  [1:0]  bshift,
-             output wire [31:0] a_desplazado);
+             output wire [4:0] a_desplazado);
 
   assign a_desplazado = a << bshift;
 
